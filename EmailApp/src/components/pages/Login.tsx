8c0881f30ced4f0a57534d8b1286bb1011c14174
Minh Anthony Tat <minhanthonytat@gmail.com>
2025-09-23 10:06:09 +1000

    login page forms missing on submit function

--- EmailApp/src/components/pages/Login.tsx
+++ EmailApp/src/components/pages/Login.tsx
@@ -7,35 +7,184 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const Login = () => {
   return (
     <div>
-      <Tabs defaultValue="login" className="w-[400px]">
+      <Tabs defaultValue="login">
         <TabsList>
-          <TabsTrigger value="login">Account</TabsTrigger>
-          <TabsTrigger value="signup">Password</TabsTrigger>
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
           <Card>
             <CardHeader>
               <CardTitle>Login</CardTitle>
               <CardDescription>
-                Enter your email below to login to your account
+                Enter your email and password below to login to your account
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <LoginForm />
             </CardContent>
             <CardFooter>
-              <p>Card Footer</p>
+              <Button>Login</Button>
             </CardFooter>
           </Card>
         </TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle>Sign Up</CardTitle>
+              <CardDescription>
+                Create new account by entering a new email and password.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <SignupForm />
+            </CardContent>
+            <CardFooter>
+              <Button>Sign Up</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
       </Tabs>
     </div>
   );
 };
 
 export default Login;
+
+const loginFormSchema = z.object({ email: z.email(), password: z.string() });
+
+const signupFormSchema = z
+  .object({
+    email: z.email(),
+    password: z.string(),
+    confirm: z.string(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+  });
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onFormSubmit = (data: z.infer<typeof loginFormSchema>) => {
+    // GET login
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+const SignupForm = () => {
+  const form = useForm<z.infer<typeof signupFormSchema>>({
+    resolver: zodResolver(signupFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirm: "",
+    },
+  });
+
+  const onFormSubmit = (data: z.infer<typeof signupFormSchema>) => {
+    // Post create new user
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Confirm" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
