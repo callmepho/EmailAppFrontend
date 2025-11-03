c2b831b0799550323152371e133b8f697c5f3490
Minh Anthony Tat <minhanthonytat@gmail.com>
2025-11-04 10:45:25 +1100

    useUserQueries didnt save

--- EmailApp/src/query/useUserQueries.tsx
+++ EmailApp/src/query/useUserQueries.tsx
@@ -1,5 +1,10 @@
-import { loginUser, registerUser } from "@/services/userQueries";
-import { useMutation } from "@tanstack/react-query";
+import {
+  deleteUser,
+  fetchCurrentUser,
+  loginUser,
+  registerUser,
+} from "@/services/userQueries";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useLoginUser = () => {
   return useMutation({
@@ -11,5 +16,18 @@ export const useRegisterUser = () => {
   return useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       registerUser(email, password),
+  });
+};
+
+export const useCurrentUser = () => {
+  return useQuery({
+    queryKey: ["currentUser"],
+    queryFn: () => fetchCurrentUser,
+  });
+};
+
+export const useDeleteUser = () => {
+  return useMutation({
+    mutationFn: ({ id }: { id: number }) => deleteUser(id),
   });
 };
